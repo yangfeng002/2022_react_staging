8133de43d2eef8faf279fe266ba388bfa6b6f70c
杨凤 <yangfeng075@chinasoftinc.com>
2022-11-23 15:59:58 +0800

    底部选中功能部分实现

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,16 +6,16 @@ export default class Footer extends Component {
   render() {
     const {todos} = this.props
 
-    const todoCount = todos.reduce(() => {
-      
-    })
+    const doneCount = todos.reduce((preValue,current) => {
+      return preValue + (current.done === true ? 1: 0)
+    }, 0)
     return (
       <div className="todo-footer">
         <label>
           <input type="checkbox"/>
         </label>
         <span>
-          <span>已完成 </span> / 全部 {todos.length}
+          <span>已完成 {doneCount}</span> / 全部 {todos.length}
         </span>
         <button className="btn btn-danger">清除已完成任务</button>
       </div>
