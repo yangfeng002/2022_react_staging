b3629aa23b2137f465808f1ffa8f1f94e22a0b20
杨凤 <yangfeng075@chinasoftinc.com>
2023-01-12 13:38:51 +0800

    路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
 import React, { Component } from 'react'
-import Search from './components/Search'
-import List from './components/List'
+import Home from './components/Home'
+import Abpout from './components/About'
 export default class App extends Component {
   state = {
-    users: [],
-    isFirst: true, // 是否为初始展示
-    isLoading: false, // 标识是否为加载中
-    errorMsg: '' // 错误信息存储
-  }
-  // saveUsers = (users) => {
-  //   this.setState({
-  //     users: users
-  //   })
-  // }
-  updateAppState = (obj) => {
-    this.setState(obj)
   }
   render() {
     return (
       <div className="container">
-       <Search updateAppState={this.updateAppState}/>
-       <List {...this.state}/>
-    </div>
+        <Home></Home>
+        <Abpout></Abpout>
+      </div>
     )
   }
 }
